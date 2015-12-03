c28f42718f4ec11c287120c7cdc13361b82a439d
jatindra <sutharjatindra@gmail.com>
2015-12-03 12:05:34 +0530

    New Files

---
+++ collections.js
@@ -0,0 +1,4 @@
+/**
+ * Created by administrator on 3/12/15.
+ */
+Todos = new Meteor.Collection('todos');
--- demoapplication.js
+++ demoapplication.js
@@ -1,23 +1,19 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Template.hello.greeting = function () {
+        return "Welcome to getting-started-meteor.";
+    };
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+        'click input' : function () {
+            // template data, if any, is available in 'this'
+            if (typeof console !== 'undefined')
+                console.log("You pressed the button");
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
